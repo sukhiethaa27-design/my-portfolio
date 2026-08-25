96e66ffdf5860ce0d452b5a2aad4ba3df8388d19
sukhiethaa27-design <sukhiethaa27@gmail.com>
2026-08-25 17:30:22 +0530

    pic change

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -3,12 +3,10 @@ import './AboutSection.css';
 function AboutSection() {
   return (
     <section id='about' className='about-section'>
-      <div className='about-wrapper container'>
-          <img src='/Photo.jpeg' alt='Sukhiethaa.S ' />
-        </div>
+      <div className='about-content'>
         <div className='about-copy'>
           <h1>About Me</h1>
-          <p>Hi! I'm Sukhiethaa.S, a Computer Science Engineering student constantly learning and building my skills in the world of technology.</p>
+          <p>Hi! I'm Sukhiethaa.S, a Computer Science Engineering student constantly learning and building my skills.</p>
         </div>
       </div>
     </section>
